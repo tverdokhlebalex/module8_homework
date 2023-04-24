3270cad01e64d0046b5039dd1269b2429190cc5e
Alexander <tverdokhlebalex@me.com>
2023-04-24 23:28:29 +0400

    final

--- ex-3.js
+++ ex-3.js
@@ -1,26 +1,24 @@
-const input = document.querySelector('input');
-const button = document.querySelector('button');
+const input = document.getElementById('number');
+const btn = document.getElementById('btn');
 const output = document.querySelector('.output');
 
-document.addEventListener("DOMContentLoaded", function() {
-button.addEventListener('click', () => {
-  const value = parseInt(input.value);
-  
-  if (value <= 0 || value > 10 || Number.isNaN(value)) {
-    output.textContent = 'Число вне диапазона от 1 до 10';
-  } else {
-    const url = `https://picsum.photos/v2/list?limit=${value}`;
-    const xhr = new XMLHttpRequest();
-    xhr.open('GET', url, true);
-    xhr.onload = function () {
-      const response = JSON.parse(xhr.responseText);
-      response.forEach(item => {
-        const img = document.createElement('img');
-        img.src = item.download_url;
-        output.appendChild(img);
-      });
-    };
-    xhr.send();
+btn.addEventListener('click', () => {
+  const inputValue = parseInt(input.value);
+  if (isNaN(inputValue)) {
+    output.innerHTML = '<p class="error">Ошибка: введите число</p>';
+    return;
   }
-});
+  if (inputValue < 1 || inputValue > 10) {
+    output.innerHTML = '<p class="error">Ошибка: число должно быть от 1 до 10</p>';
+    return;
+  }
+  
+  output.innerHTML = '';
+
+  for (let i = 1; i <= inputValue; i++) {
+    const img = document.createElement('img');
+    img.src = `https://picsum.photos/id/${i}/200`;
+    img.classList.add('img');
+    output.appendChild(img);
+  }
 });
